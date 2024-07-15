4b205e87aee4536fdd8b669bd5c2510999bc506c
amir <cavalier7654@gmail.com>
2024-07-16 00:19:37 +0330

    change btf

--- src/components/Home/Application/AppDownload.jsx
+++ src/components/Home/Application/AppDownload.jsx
@@ -3,7 +3,9 @@ export default function AppDownload({ url, title, description }) {
 		<div className="bg-white flex py-2 rounded-xl gap-4 justify-evenly items-center min-w-48">
 			<div className="flex flex-col gap-2">
 				<h1 className="font-danablack text-sm">{title}</h1>
-				<p className="text-xs font-danalight text-slate-300">{description}</p>
+				<p className="text-xs font-danalight text-slate-300">
+					{description}
+				</p>
 			</div>
 
 			<img src={url} alt="" className="max-w-[35px] max-h-[34px]" />
