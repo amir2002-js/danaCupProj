6c5d4ea98fd7b3d0d91e0882479fc67272d6aec4
amir <cavalier7654@gmail.com>
2024-07-21 07:58:51 +0330

    prparing the infrastructure to create the ability to login with email

--- src/components/Login/Form/Form.jsx
+++ src/components/Login/Form/Form.jsx
@@ -1,11 +1,13 @@
 import CodeInp from "./CodeInp";
 import EnterNum from "./EnterNum";
 import { useState } from "react";
+import LoginByNum from "./LoginByNum";
 
-export default function Form({ notValAlert, setAlert, spiner, setSpiner }) {
+export default function Form({ setAlert, spiner, setSpiner }) {
 	const [validation, setValidation] = useState(false);
 	const [inpVla, setInpVal] = useState(0);
 	const [checknum, setCheckNum] = useState(false);
+	const [loginByNumber, setLoginByNumber] = useState(true);
 
 	const changeVal = (val) => {
 		setInpVal(val);
@@ -18,30 +20,24 @@ export default function Form({ notValAlert, setAlert, spiner, setSpiner }) {
 					? "تلفن همراه"
 					: `کدی که به شماره ${inpVla} ارسال شد وارد کنید`}
 			</p>
-			<div className="flex flex-col gap-9">
-				<EnterNum
-					validation={validation}
-					setValidation={setValidation}
-					inpVla={inpVla}
-					changeVal={changeVal}
-					checknum={checknum}
-				/>
-				<div className={`${checknum ? "" : "hidden"}`}>
-					<CodeInp spiner={spiner} setSpiner={setSpiner} />
-				</div>
-				<button
-					className="bg-teal-600 hover:bg-teal-700 text-white py-1.5 mt-6 rounded-lg shadow shadow-black/70"
-					onClick={() => {
-						if (!validation) {
-							setAlert(false);
-						} else {
-							setCheckNum(true);
-						}
-					}}
-				>
-					ورود
-				</button>
-			</div>
+			<LoginByNum
+				validation={validation}
+				setValidation={setValidation}
+				spiner={spiner}
+				setSpiner={setSpiner}
+				inpVla={inpVla}
+				checknum={checknum}
+				changeVal={changeVal}
+				setAlert={setAlert}
+				setCheckNum={setCheckNum}
+			/>
+			<button
+				className={`text-blue-500 hover:text-purple-500 font-peydalight ${
+					!checknum ? "" : "hidden"
+				}`}
+			>
+				{loginByNumber ? "لاگین با ایمیل" : "لاگین با شماره تلفن"}
+			</button>
 		</div>
 	);
 }
---
+++ src/components/Login/Form/LoginByNum.jsx
@@ -0,0 +1,31 @@
+import CodeInp from "./CodeInp";
+import EnterNum from "./EnterNum";
+
+export default function LoginByNum({validation ,setValidation , spiner , setSpiner , inpVla,checknum,changeVal ,setAlert,setCheckNum}) {
+	return (
+		<div className="flex flex-col gap-9">
+			<EnterNum
+				validation={validation}
+				setValidation={setValidation}
+				inpVla={inpVla}
+				changeVal={changeVal}
+				checknum={checknum}
+			/>
+			<div className={`${checknum ? "" : "hidden"}`}>
+				<CodeInp spiner={spiner} setSpiner={setSpiner} />
+			</div>
+			<button
+				className="bg-teal-600 hover:bg-teal-700 text-white py-1.5 mt-6 rounded-lg shadow shadow-black/70"
+				onClick={() => {
+					if (!validation) {
+						setAlert(false);
+					} else {
+						setCheckNum(true);
+					}
+				}}
+			>
+				ورود
+			</button>
+		</div>
+	);
+}
