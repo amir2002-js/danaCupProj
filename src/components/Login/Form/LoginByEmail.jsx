4c922d7739f61608f2f4e89d38cf072a6cbf4048
amir <cavalier7654@gmail.com>
2024-07-23 14:39:10 +0330

    change in gitIgnore file

--- src/components/Login/Form/LoginByEmail.jsx
+++ src/components/Login/Form/LoginByEmail.jsx
@@ -1,37 +1,49 @@
-
 import EnterEmail from "./EnterEmail";
 import CodeInp from "./CodeInp";
 
-export default function LoginByEmail({emailVal , emailValidation , setEmailValidation , setEmailVal , setAlert , checkEmail , setCheckEmail , spiner , setSpiner}) {
-// export default function LoginByEmail({emailVal , emailValidation , setEmailValidation , setEmailVal , setAlert  }) {
+export default function LoginByEmail({
+	emailVal,
+	emailValidation,
+	setEmailValidation,
+	setEmailVal,
+	setAlert,
+	checkEmail,
+	setCheckEmail,
+	spiner,
+	setSpiner,
+}) {
+	// export default function LoginByEmail({emailVal , emailValidation , setEmailValidation , setEmailVal , setAlert  }) {
 
 	return (
 		<>
-			<p className={`${checkEmail? "text-sm" : " "  } text-center`}>
-
-				{checkEmail? 
-				`کدی را که به ایمیل ${emailVal} ارسال کردیم وارد کنید`
-				:
-				"ایمیل خود را وارد کنید" }
+			<p className={`${checkEmail ? "text-sm" : " "} text-center`}>
+				{checkEmail
+					? `کدی را که به ایمیل ${emailVal} ارسال کردیم وارد کنید`
+					: "ایمیل خود را وارد کنید"}
 			</p>
 			<div className="flex flex-col gap-9">
-	
-				<EnterEmail emailVal={emailVal} setEmailVal={setEmailVal} emailValidation={emailValidation} setEmailValidation={setEmailValidation} checkEmail={checkEmail}/>
+				<EnterEmail
+					emailVal={emailVal}
+					setEmailVal={setEmailVal}
+					emailValidation={emailValidation}
+					setEmailValidation={setEmailValidation}
+					checkEmail={checkEmail}
+				/>
 
 				<div className={`${checkEmail ? "" : "hidden"}`}>
 					<CodeInp spiner={spiner} setSpiner={setSpiner} />
 				</div>
-				
+
 				<button
 					className="bg-teal-600 hover:bg-teal-700 text-white py-1.5 mt-6 rounded-lg shadow shadow-black/70"
 					onClick={() => {
-							if(emailValidation){
-								setCheckEmail(true)
-							}
-							else{
-								setAlert(false)
-							}
-					}}>
+						if (emailValidation) {
+							setCheckEmail(true);
+						} else {
+							setAlert(false);
+						}
+					}}
+				>
 					ورود
 				</button>
 			</div>
